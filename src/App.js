1bd1c632b3693da98d8b13ff17a32a9ce1c4828b
Joe <joetanchinyong@gmail.com>
2019-02-08 18:00:01 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {
+  Container,
+  Grid,
+  Header,
+  Icon,
+  Input,
+  List,
+  Form
+} from 'semantic-ui-react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {currentValue: '', list: []};
+  }
+
+  componentDidMount() {
+    this.getFromLocalStorage();
+  }
+
+  getFromLocalStorage() {
+    if (localStorage.hasOwnProperty('list')) {
+      let valueList = localStorage.getItem('list');
+      this.setState({list: JSON.parse(valueList)});
+    }
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('list', JSON.stringify(this.state.list));
+  }
+
+  populateList() {
+    let items = [];
+    let valueList = this.state.list;
+    for (let index = 0; index < valueList.length; index++) {
+      const element = valueList[index];
+      items.push(<List.Item key={index}>{element}</List.Item>);
+    }
+    return items;
+  }
+
+  addToList = (e) => {
+    e.preventDefault();
+    let valueList = this.state.list;
+    valueList.push(this.state.currentValue);
+    this.setState({list: valueList});
+    this.saveToLocalStorage();
+  };
+
+  handleInputChange = (e) => {
+    this.setState({currentValue: e.target.value.split('?')[0]});
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Container>
+          <Grid centered padded columns={1} divided>
+            <Grid.Row>
+              <Grid.Column>
+                <Header as="h1" icon textAlign="center">
+                  <Icon name="facebook" />
+                  <Header.Content>Facebook</Header.Content>
+                </Header>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <Form onSubmit={this.addToList}>
+                  <Input
+                    defaultValue={this.state.currentValue}
+                    onChange={this.handleInputChange}
+                    fluid
+                    required
+                    placeholder="Link"
+                    type="url"
+                    action={{
+                      icon: 'add',
+                      type: 'submit'
+                    }}
+                  />
+                </Form>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <List>{this.populateList()}</List>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
       </div>
     );
   }
