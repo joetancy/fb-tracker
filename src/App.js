28727bc340dbe13dc8f1c0f74ef8b941022b64f1
Joe <joetanchinyong@gmail.com>
2019-02-19 14:34:29 +0800

    dev

--- src/App.js
+++ src/App.js
@@ -1,41 +1,89 @@
 import React, {Component} from 'react';
+import firebase from 'firebase';
+import * as firebaseui from 'firebaseui';
+import _ from 'lodash';
+import moment from 'moment';
 import {
   Container,
   Grid,
   Header,
   Icon,
   Input,
-  Card,
   Form,
   Button,
   Select,
-  Loader
+  Loader,
+  Table,
+  Segment,
+  Label
 } from 'semantic-ui-react';
-import MetricCard from './components/MetricCard';
-import Swal from 'sweetalert2';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      accessToken: '',
       currentValue: '',
       currentPage: '',
-      links: [],
-      oldLinks: [],
+      data: [],
+      oldData: [],
       facebookData: [],
       items: [],
-      loading: false
+      loading: false,
+      appUserID: '',
+      column: null,
+      direction: null
     };
     this.loadFacebook = this.loadFacebook.bind(this);
-    this.clearLocalStorage = this.clearLocalStorage.bind(this);
     this.addToList = this.addToList.bind(this);
     this.forceRefresh = this.forceRefresh.bind(this);
-    this.backupRestore = this.backupRestore.bind(this);
+    this.firebaseUI = this.firebaseUI.bind(this);
+    this.deleteRow = this.deleteRow.bind(this);
+    this.signOut = this.signOut.bind(this);
   }
 
   componentDidMount() {
-    this.getFromLocalStorage();
     this.loadFacebook();
+    var config = {
+      apiKey: 'AIzaSyCi8s6wYR8LT2hTLwOgEAmduO_3qZE_ohE',
+      authDomain: 'facebooktest-f856a.firebaseapp.com',
+      projectId: 'facebooktest-f856a'
+    };
+    firebase.initializeApp(config);
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.loginFacebook();
+        this.setState({appUserID: user.uid});
+        this.setState({loading: true});
+      } else {
+        this.firebaseUI();
+      }
+    });
+  }
+
+  firebaseUI() {
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    var uiConfig = {
+      callbacks: {
+        signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+          this.setState({accessToken: authResult.credential.accessToken});
+          this.loginFacebook();
+          return false;
+        }
+      },
+      signInFlow: 'popup',
+      signInOptions: [
+        {
+          provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+          scopes: ['manage_pages', 'read_insights']
+        }
+      ]
+    };
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }
+
+  signOut() {
+    firebase.auth().signOut();
   }
 
   loadFacebook() {
@@ -59,41 +107,54 @@ class App extends Component {
         version: 'v3.2',
         state: true
       });
-      this.loginFacebook();
     };
   }
 
   loginFacebook() {
     window.FB.getLoginStatus((response) => {
       if (response.status === 'connected') {
-        window.FB.api('/me/accounts?fields=name,access_token', (response) => {
-          this.setState({facebookData: response.data});
-          this.populateSelect();
-        });
+        window.FB.api(
+          '/me/accounts?fields=id,name,access_token',
+          (response) => {
+            this.setState({facebookData: response.data});
+            this.getFromDB();
+            this.populateSelect();
+          }
+        );
       } else {
         console.log('Login Failed!');
       }
     });
   }
 
-  clearLocalStorage() {
-    if (window.confirm('Clear all history?')) {
-      localStorage.clear();
-      this.setState({links: []});
-    }
+  getFromDB() {
+    const db = firebase.firestore();
+    var docRef = db.collection('records').doc(this.state.appUserID);
+    docRef
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          this.setState({loading: false, data: doc.data().data});
+        } else {
+          console.log('No such document!');
+          this.setState({loading: false});
+        }
+      })
+      .catch((error) => {
+        console.log('Error getting document', error);
+      });
   }
 
-  getFromLocalStorage() {
-    if (localStorage.hasOwnProperty('links')) {
-      let valueList = localStorage.getItem('links');
-      this.setState({links: JSON.parse(valueList)});
-    } else {
-      this.setState({links: []});
-    }
-  }
-
-  saveToLocalStorage() {
-    localStorage.setItem('links', JSON.stringify(this.state.links));
+  saveToDB() {
+    const db = firebase.firestore();
+    db.collection('records')
+      .doc(this.state.appUserID)
+      .set(
+        {
+          data: this.state.data
+        },
+        {merge: true}
+      );
   }
 
   loader() {
@@ -104,27 +165,9 @@ class App extends Component {
     }
   }
 
-  backupRestore() {
-    Swal.fire({
-      title: 'Copy the text contents and save to a file for backup.',
-      input: 'textarea',
-      inputValue: JSON.stringify(this.state.links),
-      inputPlaceholder: 'Type your message here...',
-      showCancelButton: true,
-      confirmButtonText: 'Restore'
-    })
-      .then((result) => {
-        if (result.value !== undefined) {
-          this.setState({links: JSON.parse(result.value)});
-          this.saveToLocalStorage();
-        }
-      })
-      .catch();
-  }
-
   forceRefresh() {
-    this.setState({oldLinks: this.state.links, links: []});
-    this.state.links.forEach((element) => {
+    this.setState({oldData: this.state.data, data: []});
+    this.state.data.forEach((element) => {
       this.getMetricsRefresh(element);
     });
   }
@@ -142,27 +185,6 @@ class App extends Component {
       return a.text.localeCompare(b.text);
     });
     this.setState({facebookData: pages});
-  }
-
-  populateList() {
-    let items = [];
-    this.state.links.forEach((element) => {
-      items.push(
-        <MetricCard
-          key={element.postId}
-          type={element.type}
-          pageName={element.pageName}
-          impressions={element.impressions}
-          reach={element.reach}
-          engagement={element.engagement}
-          link={element.link}
-          name={element.name}
-          message={element.message}
-          created_time={element.created_time}
-        />
-      );
-    });
-    return items;
   }
 
   addToList = (e) => {
@@ -230,11 +252,10 @@ class App extends Component {
             engagement: response.insights.data[2].values[0].value
           };
         }
-        let valueList = this.state.links;
+        let valueList = this.state.data;
         valueList.push(postData);
-        this.setState({links: valueList});
-        this.saveToLocalStorage();
-        this.populateList();
+        this.setState({data: valueList});
+        this.saveToDB();
         this.setState({loading: false});
       }
     );
@@ -307,13 +328,57 @@ class App extends Component {
           };
         }
 
-        let valueList = this.state.links;
+        let valueList = this.state.data;
         valueList.push(postData);
-        this.setState({links: valueList});
-        this.saveToLocalStorage();
-        this.populateList();
+        this.setState({data: valueList});
+        this.saveToDB();
       }
     );
+  };
+
+  deleteRow = (postId) => {
+    let data = this.state.data;
+    let oldData = _.remove(data, (element) => {
+      if (element.postId === postId) {
+        return true;
+      }
+    });
+    this.setState({data: data});
+    this.saveToDB();
+    const db = firebase.firestore();
+    var docRef = db.collection('records').doc(this.state.appUserID);
+    docRef
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          if (doc.data().archive !== undefined) {
+            let archive = doc.data().archive;
+            archive.push(oldData[0]);
+            db.collection('records')
+              .doc(this.state.appUserID)
+              .set(
+                {
+                  archive: archive
+                },
+                {merge: true}
+              );
+          } else {
+            db.collection('records')
+              .doc(this.state.appUserID)
+              .set(
+                {
+                  archive: oldData
+                },
+                {merge: true}
+              );
+          }
+        } else {
+          console.log('No such document!');
+        }
+      })
+      .catch((error) => {
+        console.log('Error getting document', error);
+      });
   };
 
   handleInputChange = (e) => {
@@ -326,30 +391,70 @@ class App extends Component {
     });
   };
 
+  handleSort = (clickedColumn) => () => {
+    const {column, data, direction} = this.state;
+    if (column !== clickedColumn) {
+      this.setState({
+        column: clickedColumn,
+        data: _.sortBy(data, [clickedColumn]),
+        direction: 'ascending'
+      });
+
+      return;
+    }
+    this.setState({
+      data: data.reverse(),
+      direction: direction === 'ascending' ? 'descending' : 'ascending'
+    });
+  };
+
   render() {
+    const {column, direction} = this.state;
     return (
       <div className="App">
-        <Container>
-          <Grid centered padded columns={1} divided>
-            <Grid.Row>
-              <Grid.Column>
+        <Container fluid>
+          <Grid centered padded columns={3}>
+            <Grid.Row centered>
+              <Grid.Column width={3} />
+              <Grid.Column width={10}>
                 <Header as="h1" icon textAlign="center">
                   <Icon name="facebook" />
                   <Header.Content>Facebook Insights Tracker</Header.Content>
                 </Header>
-                <div
-                  className="fb-login-button"
-                  style={{display: 'block', textAlign: 'center'}}
-                  data-size="large"
-                  data-button-type="continue_with"
-                  data-auto-logout-link="true"
-                  data-use-continue-as="true"
-                  data-scope="read_insights, manage_pages"
-                />
+                <div id="firebaseui-auth-container" />
+              </Grid.Column>
+              <Grid.Column width={3}>
+                <Segment>
+                  <Header>Legend</Header>
+                  <>
+                    <Label circular color="red" empty />
+                    {' < 25,000'}
+                  </>
+                  <br />
+                  <>
+                    <Label circular color="orange" empty />
+                    {' < 50,000'}
+                  </>
+                  <br />
+                  <>
+                    <Label circular color="blue" empty />
+                    {' < 100,000'}
+                  </>
+                  <br />
+                  <>
+                    <Label circular color="green" empty />
+                    {' < 500,000'}
+                  </>
+                  <br />
+                  <>
+                    <Icon name="star outline" fitted />
+                    {' > 500,000'}
+                  </>
+                </Segment>
               </Grid.Column>
             </Grid.Row>
             <Grid.Row>
-              <Grid.Column>
+              <Grid.Column width={10}>
                 <Form onSubmit={this.addToList}>
                   <Input
                     defaultValue={this.state.currentValue}
@@ -373,29 +478,126 @@ class App extends Component {
               </Grid.Column>
             </Grid.Row>
             <Grid.Row>
-              <Grid.Column>
-                <Button
-                  negative
-                  content="Clear History"
-                  onClick={this.clearLocalStorage}
-                />
+              <Grid.Column width={10}>
                 <Button
                   positive
                   content="Force Refresh"
                   onClick={this.forceRefresh}
                 />
-                <Button
-                  content="Backup & Restore"
-                  onClick={this.backupRestore}
-                />
+                <Button content="Sign Out" onClick={this.signOut} />
               </Grid.Column>
             </Grid.Row>
             <Grid.Row>
-              <Grid.Column>
+              <Grid.Column width={10}>
                 {this.loader()}
-                <Card.Group centered itemsPerRow="3">
-                  {this.populateList()}
-                </Card.Group>
+                <Table striped compact sortable fixed singleLine>
+                  <Table.Header>
+                    <Table.Row>
+                      <Table.HeaderCell>Page</Table.HeaderCell>
+                      <Table.HeaderCell>Name</Table.HeaderCell>
+                      {/* <Table.HeaderCell>Message</Table.HeaderCell> */}
+                      <Table.HeaderCell
+                        sorted={column === 'impressions' ? direction : null}
+                        onClick={this.handleSort('impressions')}
+                      >
+                        Impressions
+                      </Table.HeaderCell>
+                      <Table.HeaderCell
+                        sorted={column === 'reach' ? direction : null}
+                        onClick={this.handleSort('reach')}
+                      >
+                        Reach
+                      </Table.HeaderCell>
+                      <Table.HeaderCell
+                        sorted={column === 'engagement' ? direction : null}
+                        onClick={this.handleSort('engagement')}
+                      >
+                        Engagement
+                      </Table.HeaderCell>
+                      <Table.HeaderCell>Days Posted</Table.HeaderCell>
+                      <Table.HeaderCell>Action</Table.HeaderCell>
+                    </Table.Row>
+                  </Table.Header>
+                  <Table.Body>
+                    {_.map(
+                      this.state.data,
+                      ({
+                        pageName,
+                        postId,
+                        name,
+                        // message,
+                        impressions,
+                        reach,
+                        engagement,
+                        link,
+                        type,
+                        created_time
+                      }) => (
+                        <Table.Row>
+                          <Table.Cell>{pageName}</Table.Cell>
+                          <Table.Cell>{name}</Table.Cell>
+                          {/* <Table.Cell>{message}</Table.Cell> */}
+                          <Table.Cell>
+                            {impressions.toLocaleString()}
+                          </Table.Cell>
+                          <Table.Cell>
+                            {reach < 25000 ? (
+                              <Header as="h5" color="red">
+                                {reach.toLocaleString()}
+                              </Header>
+                            ) : reach < 50000 ? (
+                              <Header as="h5" color="orange">
+                                {reach.toLocaleString()}
+                              </Header>
+                            ) : reach < 100000 ? (
+                              <Header as="h5" color="blue">
+                                {reach.toLocaleString()}
+                              </Header>
+                            ) : reach < 500000 ? (
+                              <Header as="h5" color="green">
+                                {reach.toLocaleString()}
+                              </Header>
+                            ) : (
+                              <>
+                                {reach.toLocaleString()}
+                                <Icon name="star outline" />
+                              </>
+                            )}
+                          </Table.Cell>
+                          <Table.Cell>
+                            <p>
+                              {type === 'video'
+                                ? 'Video Views '
+                                : 'Post Clicks '}
+                            </p>
+                            {engagement.toLocaleString()}
+                          </Table.Cell>
+                          <Table.Cell>
+                            {moment(
+                              created_time,
+                              'DD/MM/YYYY h:mm a'
+                            ).fromNow()}
+                          </Table.Cell>
+                          <Table.Cell textAlign="center">
+                            <a
+                              href={link}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              <Button basic color="blue" icon="eye" />
+                            </a>
+                            <Button
+                              basic
+                              color="red"
+                              icon="trash alternate outline"
+                              onClick={() => this.deleteRow(postId)}
+                            />
+                          </Table.Cell>
+                        </Table.Row>
+                      )
+                    )}
+                  </Table.Body>
+                </Table>
               </Grid.Column>
             </Grid.Row>
           </Grid>
